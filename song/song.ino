1a22031a62f3b5523a8e1a2bd6f6a18775b9e718
Cheng-Shu-Chang <emc2chang@gmail.com>
2022-09-13 19:31:34 +0800

    modify the calculation of beat

--- song/song.ino
+++ song/song.ino
@@ -1,4 +1,3 @@
-#include<string.h>
 #include<math.h>
 
 int buzzer = 11;
@@ -8,61 +7,75 @@ void setup() {
   pinMode(buzzer, OUTPUT);
   Serial.begin(9600);
 }
+//String a = "5#";
 
-char song[] = "671.71.3.736561.534341.31.74#4#77";
-char beat[] = "884.8442.44.8442.44.884.2.44.842.";
-unsigned int oneBar = 60000/40*4;
+
+
 
 void loop() {
   // put your main code here, to run repeatedly:
+  //Serial.print(a.length());
+  //Serial.print("\n");
+  String song = "671.71.3.736561.534341.31.74#4#77671.71.3.7336561.5341.71.2.3.1.1.7676b6";
+  String beat = "11312262312262311362312261131226113122622132215112226";
+  float oneBeat = 60000/87;
   int j = 0;
-  for(int i=0;i<strlen(song);i++){
-    char note[3]={0};
-    if(song[i+1]=='.' || song[i+1]=='#'){
-      note[0]=song[i];
-      note[1]=song[i+1];
+  delay(100);
+  Serial.print('\n');
+  Serial.print(beat.length());
+  Serial.print('\n');
+  String note = "";
+  for(int i=0;i<song.length();i++){
+    if(song[i+1]=='.' || song[i+1]=='#'|| song[i+1]=='b'){
+      note = song.substring(i,i+2);
       i++;
     }else{
-      note[0]=song[i]; 
+      note = song.substring(i,i+1);
     }
     
     int p=0;
-    if(strcmp(note,"1")==0){
+    if(note=="1"){
       p = 60;
-    }else if(strcmp(note,"2")==0){
+    }else if(note=="2"){
       p = 62;
-    }else if(strcmp(note,"3")==0){
+    }else if(note=="3"){
       p = 64;
-    }else if(strcmp(note,"4")==0){
+    }else if(note=="4"){
       p = 65;
-    }else if(strcmp(note,"4#")==0){
+    }else if(note=="4#"){
       p = 66;
-    }else if(strcmp(note,"5")==0){
+    }else if(note=="5"){
       p = 67;
-    }else if(strcmp(note,"6")==0){
+    }else if(note=="6b"){
+      p = 68;
+    }else if(note=="6"){
       p = 69;
-    }else if(strcmp(note,"7")==0){
+    }else if(note=="7"){
       p = 71;
-    }else if(strcmp(note,"1.")==0){
+    }else if(note=="1."){
       p = 72;
-    }else if(strcmp(note,"2.")==0){
+    }else if(note=="2."){
       p = 74;
-    }else if(strcmp(note,"3.")==0){
+    }else if(note=="3."){
       p = 76;
     }
-  
-    float duration;
-    if(beat[j+1]=='.'){
-      duration = 1./int(beat[j]-'0')+1./2/int(beat[j]-'0');
-      duration *= oneBar;
-      j++;
-    }else{
-      duration = 1./int(beat[j]-'0');
-      duration *= oneBar;
-    }
+
     
-    tone(buzzer, int(pow(2,(p-69)/12.0)*440), int(duration));
-    delay(oneBar/4*0.2);
+    float duration;
+    duration = oneBeat/2*(beat[j]-'0');
+    Serial.print(i);
+    Serial.print(" ");
+    Serial.print(note);
+    Serial.print(" ");
+    Serial.print(j);
+    Serial.print(" ");
+    Serial.print(duration);
+    Serial.print("\n");
+    tone(buzzer, int(pow(2,(p-69)/12.0)*440), long(duration*0.9));
+    delay(duration);
+    
     j++;
   }
+  noTone(buzzer);
+  delay(1000);
 }
